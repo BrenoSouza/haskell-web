c9d16b6cba769b347ed5686c120c85c7a8508544
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2018-07-22 15:17:47 -0300

    Inicializacao do server

---
+++ src/Main.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Web.Scotty
+
+main :: IO ()
+main = do
+  putStrLn "Starting Server..."
+  scotty 3000 $ do
+    get "/heap" $ do
+        text "hello world!"
